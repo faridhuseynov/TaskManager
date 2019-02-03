15aa68a9a1a69b040f9a59bf66170ff7ef4b959a
Farid Huseynov <F.H189@hotmail.com>
2019-02-03 16:13:34 +0400

    TaskItem and WPF View drafted

---
+++ TaskManager/TaskItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    class TaskItem
+    {
+        public string Name { get; set; }
+        public int PID { get; set; }
+        public string Status { get; set; }
+        public string UserName { get; set; }
+        public string CPU { get; set; }
+        public string Memory { get; set; }
+        public string Description { get; set; }
+    }
+}
