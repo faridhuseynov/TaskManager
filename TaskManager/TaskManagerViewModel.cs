6f2a4cbec93f51d9ef55db59ea4cad46de72a6fd
Farid Huseynov <F.H189@hotmail.com>
2019-02-03 20:38:30 +0400

    Adding and Ending of tasks done.
    Timer and listviewupdate should be fixed

--- TaskManager/TaskManagerViewModel.cs
+++ TaskManager/TaskManagerViewModel.cs
@@ -6,35 +6,23 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Management;
+using System.Dynamic;
+using GalaSoft.MvvmLight.CommandWpf;
+using System.Windows;
 
 namespace TaskManager
 {
     class TaskManagerViewModel : ViewModelBase
     {
-        private string taskName;
-        public string TaskName { get => taskName; set => Set(ref taskName, value); }
-
-        private string pid;
-        public string PID { get => pid; set => Set(ref pid, value); }
-
-
-        private string status;
-        public string Status { get => status; set => Set(ref status, value); }
-
-        private string userName;
-        public string UserName { get => userName; set => Set(ref userName, value); }
-
-        private string cpu;
-        public string CPU { get => cpu; set => Set(ref cpu, value); }
-
-        private string memory;
-        public string Memory { get => memory; set => Set(ref memory, value); }
-
-        private string description;
-        public string Description { get => description; set => Set(ref description, value); }
-
         private ObservableCollection<TaskItem> taskList;
         public ObservableCollection<TaskItem> TaskList { get => taskList; set => Set(ref taskList, value); }
+
+        private string newTaskRequest;
+        public string NewTaskRequest { get => newTaskRequest; set => Set(ref newTaskRequest, value); }
+
+        private TaskItem selectedItem;
+        public TaskItem SelectedItem { get => selectedItem; set => Set(ref selectedItem, value); }
 
         public TaskManagerViewModel()
         {
@@ -42,17 +30,98 @@ namespace TaskManager
             Process[] processes = Process.GetProcesses();
             for (int i = 0; i < processes.Length; i++)
             {
-                TaskList.Add(new TaskItem
+                var newTask = new TaskItem();
+                newTask.Name = processes[i].ProcessName.ToString();
+                newTask.PID = processes[i].Id;
+                newTask.UserName = GetProcessExtraInformation(newTask.PID);
+                try
                 {
-                    Name = processes[i].ProcessName.ToString(),
-                    PID=processes[i].Id,
-                    Status=processes[i].get
-                    CPU=processes[i].TotalProcessorTime.ToString(),
-                    Memory=
-
-                });
+                newTask.CPU = $"{processes[i].TotalProcessorTime.ToString()[0]}{processes[i].TotalProcessorTime.ToString()[1]}";
+                newTask.Description = processes[i].MainModule.FileVersionInfo.FileDescription;
+                }
+                catch (Exception)
+                {
+                    
+                }
+                newTask.Memory = (Convert.ToDouble(processes[i].PrivateMemorySize64) / 1024).ToString() + " K";
+                TaskList.Add(newTask);
             }
 
+        }
+        public string GetProcessExtraInformation(int processId)
+        {
+            // Query the Win32_Process
+            string query = "Select * From Win32_Process Where ProcessID = " + processId;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            ManagementObjectCollection processList = searcher.Get();
+
+            // Create a dynamic object to store some properties on it
+            dynamic response = new ExpandoObject();
+            response.Description = "";
+            response.Username = "Unknown";
+
+            foreach (ManagementObject obj in processList)
+            {
+                // Retrieve username 
+                string[] argList = new string[] { string.Empty, string.Empty };
+                int returnVal = Convert.ToInt32(obj.InvokeMethod("GetOwner", argList));
+                if (returnVal == 0)
+                {
+                    // return Username
+                   return response.Username = argList[0];
+
+                    // You can return the domain too like (PCDesktop-123123\Username using instead
+                    //response.Username = argList[1] + "\\" + argList[0];
+                }
+            }
+
+            return "N/A";
+        }
+
+        private RelayCommand endTaskCommand;
+        public RelayCommand EndTaskCommand
+        {
+            get => endTaskCommand ?? (endTaskCommand = new RelayCommand(
+               () =>
+                {
+                    var request = Process.GetProcessById(SelectedItem.PID);
+                    if (request!=null)
+                    {
+                        request.Kill();
+                        Application.Current.Dispatcher.Invoke(() =>
+                        {
+                            TaskList.Remove(SelectedItem);
+                        });
+                        SelectedItem = null;
+                    }
+                }
+            ));
+        }
+
+        private RelayCommand startTaskCommand;
+        public RelayCommand StartTaskCommand
+        {
+            get => startTaskCommand ?? (startTaskCommand = new RelayCommand(
+               () =>
+               {
+                   if (!String.IsNullOrEmpty(NewTaskRequest))
+                   {
+                       Process.Start(NewTaskRequest);
+                       var request = Process.GetProcessesByName(NewTaskRequest);
+                       NewTaskRequest = "";
+                       if (request != null)
+                       {
+                           Application.Current.Dispatcher.Invoke(() =>
+                           {
+                               TaskList.Add(SelectedItem);
+                           });
+                       }
+                   }
+                   else
+                       MessageBox.Show("Check the task name");
+                   SelectedItem = null;
+               }
+            ));
         }
     }
 
